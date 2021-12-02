58409d547c44e5a96ce990a1eae907cb6bfedc1d
KarelAsselman <karel.asselman@allphi.eu>
2021-12-02 13:23:20 +0100

    added some white lines in script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll("[data-carousel-button]");
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
@@ -8,6 +9,7 @@ buttons.forEach((button) => {
 
     const activeSlide = slides.querySelector("[data-active]");
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
     if (newIndex < 0) newIndex = slides.children.length - 1;
     if (newIndex >= slides.children.length) newIndex = 0;
 
